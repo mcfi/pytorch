3e2b8aaf9f2d149eb923a4ec31c20a28ff1131b6
Ke Wen <kw2501@meta.com>
2025-08-20 00:50:56 -0700

    [SymmMem] Support memory pool (NVSHMEM and NCCL backends)
    ghstack-source-id: f1d329751210306d5536eef56eaf17d0a595fe3b
    Pull-Request-resolved: https://github.com/pytorch/pytorch/pull/161008

--- test/distributed/test_nvshmem.py
+++ test/distributed/test_nvshmem.py
@@ -108,6 +108,68 @@ class NVSHMEMSymmetricMemoryTest(MultiProcContinousTest):
         self.assertIs(hdl_0, hdl_1)
 
     @skipIfRocm
+    def test_mem_pool_context(self) -> None:
+        """
+        Test the effectiveness of MemPool context.
+        (More tests below to test reuseness etc.)
+        """
+        self._init_device()
+        group_name = dist.group.WORLD.group_name
+        symm_mem.enable_symm_mem_for_group(group_name)
+
+        dtype = torch.float
+        numel = 1024
+        src_rank = 0
+        dst_rank = 1
+
+        allocator = symm_mem.get_mempool_allocator(self.device)
+        mempool = torch.cuda.MemPool(allocator)
+
+        with torch.cuda.use_mem_pool(mempool):
+            if self.rank == src_rank:
+                tensor = torch.arange(numel, dtype=dtype, device=self.device)
+            else:
+                tensor = torch.zeros(numel, dtype=dtype, device=self.device)
+
+        symm_mem.rendezvous(tensor, group=group_name)
+
+        if self.rank == src_rank:
+            torch.ops.symm_mem.nvshmem_put(tensor, dst_rank)
+            # TODO: remove after we have wait_signal
+            dist.barrier()
+        else:
+            dist.barrier()
+
+        if self.rank == dst_rank:
+            self.assertEqual(
+                tensor, torch.arange(numel, dtype=dtype, device=self.device)
+            )
+
+    @skipIfRocm
+    def test_mem_pool_prev_op(self) -> None:
+        """
+        Apply MemPool context to a previous op that creates input to collective.
+        """
+        self._init_device()
+        group_name = dist.group.WORLD.group_name
+        symm_mem.enable_symm_mem_for_group(group_name)
+
+        dtype = torch.float
+        dim = 1024
+        lin = torch.nn.Linear(dim, dim, device=self.device)
+        x = torch.randn(dim, dtype=dtype, device=self.device)
+
+        allocator = symm_mem.get_mempool_allocator(self.device)
+        mempool = torch.cuda.MemPool(allocator)
+
+        with torch.cuda.use_mem_pool(mempool):
+            y = lin(x)
+            z = torch.nn.functional.relu(y)
+
+        # y and z should be both symm tensors
+        torch.ops.symm_mem.nvshmem_all_to_all(y, z, group_name)
+
+    @skipIfRocm
     def test_nvshmem_put(self) -> None:
         self._init_device()
         group_name = dist.group.WORLD.group_name
